4b29e462268cc0ac9c5203158fb99733d0dcfcf7
Victor Zuluaga <zrvictor@outlook.com>
2020-06-10 20:01:09 +0000

    to print without to use printf, puts and putchar

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -7,12 +8,7 @@
  */
 int main(void)
 {
-	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	int i;
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-	for (i = 0; s[i]; i++)
-		putchar(s[i]);
-
-putchar('\n');
-return(1);
+return (1);
 }
