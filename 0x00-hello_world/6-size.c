89568fdfee958e8c2ec8408711f09139e54572f6
Victor Zuluaga <zrvictor@outlook.com>
2020-06-10 18:47:16 +0000

    program to know size of data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Enter point
+ * print size of type data
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("Size of a char: %d byte(s) \n", sizeof(char));
+	printf("Size of a int: %d byte(s) \n", sizeof(int));
+	printf("Size of a long int: %d byte(s) \n", sizeof(long int));
+	printf("Size of a long long: %d byte(s) \n", sizeof(long long));
+	printf("Size of a float: %d byte(s) \n", sizeof(float));
+
+return (0);
+}
