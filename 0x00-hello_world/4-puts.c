c62688f481fed361d57e87384a5989d25857b973
Victor Zuluaga <zrvictor@outlook.com>
2020-06-10 17:08:50 +0000

    first program wrote in C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * main - Enter point
+ * print firtst string standard output
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
+}
