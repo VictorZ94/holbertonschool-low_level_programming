fc3a4fb8a3fc0be16368da056be0a7bee746ff87
Victor Zuluaga <zrvictor@outlook.com>
2020-06-12 02:31:45 +0000

    print numbers of the 0... until 99 with putchsr

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	while (num < 100)
+	{
+		putchar((num / 10) + '0');
+		putchar((num % 10) + '0');
+		if (num != 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	num++;
+	}
+
+	putchar('\n');
+	return (0);
+}
