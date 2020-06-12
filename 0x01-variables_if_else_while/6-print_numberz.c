5f24aa670082865b69e3dd2ef26f1122f3173e3e
Victor Zuluaga <zrvictor@outlook.com>
2020-06-12 00:32:14 +0000

    print numbers base 10!

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+
+	putchar('\n');
+	return (0);
+}
