20111703ca2828276056bd03a780c1ad732aa9f5
Victor Zuluaga <zrvictor@outlook.com>
2020-06-14 23:46:01 +0000

    print comb numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 0;
+	int div, mod, aux;
+
+	while (num < 100)
+	{
+		div = num / 10;
+		mod = num % 10;
+		aux = mod * 10 + div;
+
+		if (num != aux && num < aux)
+		{
+			putchar((num / 10) + '0');
+			putchar((num % 10) + '0');
+		
+			if (num != 89)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	num++;
+	}
+
+	putchar('\n');
+	return (0);
+}
