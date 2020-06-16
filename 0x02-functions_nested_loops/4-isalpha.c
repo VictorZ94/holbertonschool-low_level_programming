c23a194cd0300e33cf3671931e769ed4839be43b
Victor Zuluaga <zrvictor@outlook.com>
2020-06-16 02:50:46 +0000

    function verify if a cgaracter passed is lowercase or uppercase

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - verify if a character passed is lowercase or uppercase.
+ * @c: argument can be dec, char, octal or hex of the ascii code
+ * 
+ * Return: On success 1 otherwise 0.
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 172) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+return (0);
+}
