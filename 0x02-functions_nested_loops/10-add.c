ab8ed45ea5a06ba50b184f57d2324ff44a10d474
Victor Zuluaga <zrvictor@outlook.com>
2020-06-16 20:48:10 +0000

    function that add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - function that add 2 numbers.
+ * @a: first argument number
+ * @b: Second argument number
+ *
+ * Return: Add.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
