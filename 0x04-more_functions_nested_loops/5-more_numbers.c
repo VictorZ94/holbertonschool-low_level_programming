4158ff223b92a7cb17f2d3c8263e34f715cf6c6d
Victor Zuluaga <zrvictor@outlook.com>
2020-06-18 18:49:07 +0000

    print numbers until 14 with 3 putchar

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - print numbers until 14 with 3 putchar
+ *
+ * Return: Nothing
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	i = 0;
+	while (i <= 10)
+	{
+		j = 0;
+		while (j <= 14)
+		{
+			if (j > 9)
+				_putchar('1');
+
+		_putchar((j % 10) + '0');
+		j++;
+		}
+	_putchar('\n');
+	i++;
+	}
+}
