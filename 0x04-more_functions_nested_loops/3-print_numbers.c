e5a03a1a54a4202a514f8ad0a4bedfbc215c7250
Victor Zuluaga <zrvictor@outlook.com>
2020-06-18 17:17:46 +0000

    print numbers naturals

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - function print number 0 until 9
+ *
+ * Return: Nothing
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	i = 48;
+	while (i <= 57)
+	{
+		_putchar(i);
+		i++;
+	}
+_putchar('\n');
+}
