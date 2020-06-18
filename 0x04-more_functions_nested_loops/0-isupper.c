7e15847584efff291fdfa32737f4fe4590886a21
Victor Zuluaga <zrvictor@outlook.com>
2020-06-18 16:07:09 +0000

    function verify character is appercase or not)

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,18 +2,20 @@
 
 /**
  * _isupper - print verify if a character is upper or not
+ * @c: Argument type char
  *
  * Return: On success 1 otherwise 0
  */
 
 int _isupper(int c)
 {
-		if (c >= 0x41 && c <= 0x5A) /* if you want to print with octals is 0101 and 0132 */
+		/* if you want to print with octals is 0101 and 0132 */
+		if (c >= 0x41 && c <= 0x5A)
 		{
-				return (1);
+			return (1);
 		}
 		else
 		{
-				return (0);
+			return (0);
 		}
 }
