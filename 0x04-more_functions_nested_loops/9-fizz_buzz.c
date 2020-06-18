e58c6a3cda078a2b592c5ed647eb1dac7af02011
Victor Zuluaga <zrvictor@outlook.com>
2020-06-18 22:57:48 +0000

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,28 +7,28 @@
  */
 int main(void)
 {
-    int i;
+	int i;
 
-    i = 1;
-    while (i < 100)
-    {
-        if ((i % 3 == 0) && (i % 5 == 0))
-        {
-            printf("FizzBuzz ");
-        }
-        else if (i % 3 == 0)
-        {
-            printf("Fizz ");
-        }
-        else if (i % 5 == 0)
-        {
-            printf("Buzz ");
-        }
-        else
-        {
-            printf("%d ", i);
-        }
-        i++;
-    }
+	i = 1;
+	while (i < 100)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+		i++;
+	}
 return (0);
 }
