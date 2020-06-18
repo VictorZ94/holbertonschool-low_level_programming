75718aac016a3e154468c14dc7a2fac83592d220
Victor Zuluaga <zrvictor@outlook.com>
2020-06-18 16:41:51 +0000

    function times two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * mul - print verify if a character is upper or not
+ * @a: Argument int 1
+ * @b: Argument int 2
+ *
+ * Return: Multiplication of two numbers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
