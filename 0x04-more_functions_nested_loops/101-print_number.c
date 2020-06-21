018ed134d31644772640850300421c7f6845e812
Victor Zuluaga <zrvictor@outlook.com>
2020-06-21 00:01:11 +0000

    print int number with putchar

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-
+#include <stdio.h>
 /**
  * print_number - print integer number with putchar.
  * @n: Argument int number
@@ -8,18 +8,43 @@
  */
 void print_number(int n)
 {
-	/* If number is smaller than 0, put a - sign */
-	/* and change number to positive */
+
+	/*
+	 *if (n < 0)
+	 *{
+	 *_putchar('-');
+	 *n = -n;
+	 *}
+	 *if (n / 10 != 0)
+	 *print_number(n / 10);
+	 *_putchar(n % 10 + '0');
+	 **/
+	char lastDigit;
+	int reversed;
+	char c;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		lastDigit = ('0' - (n % 10));
+		n = n / -10;
 	}
-
-	/* Remove the last digit and recur */
-	if (n / 10 != 0)
-		print_number(n / 10);
-
-	/* Print the last digit */
-	_putchar(n % 10 + '0');
+	else
+	{
+		lastDigit = ((n % 10) + '0');
+		n = n / 10;
+	}
+	reversed = 0;
+	while (n > 0)
+	{
+		reversed = reversed * 10 + (n % 10);
+		n = n / 10;
+	}
+	while (reversed > 0)
+	{
+		c = ((reversed % 10) + '0');
+		_putchar(c);
+		reversed = reversed / 10;
+	}
+	_putchar(lastDigit);
 }
