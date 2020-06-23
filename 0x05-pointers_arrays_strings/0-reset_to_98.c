6e26f28b424d201cf1c016845b0a843255bdfb3f
Victor Zuluaga <zrvictor@outlook.com>
2020-06-23 15:46:08 +0000

    Deferencing a pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - Dereferencing a pointer
+ * @n: Argument pointer to int variable
+ *
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
