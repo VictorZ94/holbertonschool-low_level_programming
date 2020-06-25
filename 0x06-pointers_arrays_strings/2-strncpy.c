a5d756caf39dc17523540e243ce2a982572af212
Victor Zuluaga <zrvictor@outlook.com>
2020-06-25 20:09:37 +0000

    copy n bytes specified

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - concaten two string
+ * @dest: Argument that it recieve
+ * @src: Argument that it give
+ * @n: n bytes
+ *
+ * Return: Pointer to char
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+      int i;
+
+	i = 0;
+	while (src[i] && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+      while (i < n)
+      {
+            dest[i] = '\0';
+            i++;
+      }
+      
+return (dest);
+}
