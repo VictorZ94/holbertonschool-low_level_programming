82b3f23f2d923f2d6bb0b2e87384dabdf27b5814
Victor Zuluaga <zrvictor@outlook.com>
2020-06-25 17:26:58 +0000

    function concanate two string

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * _strcat - concaten two string 
+ * @dest: Argument that it recieve 
+ * @src: Argument that it give
+ *
+ * Return: Pointer to char
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	while(*(dest + i))
+	{
+		i++;
+	}
+
+	j = 0;
+	while(*(src + j))
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+dest[i] = '\0';
+return (dest);
+}
