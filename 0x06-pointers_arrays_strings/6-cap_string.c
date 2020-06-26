3aef660d50759150f295514e1dc0222d42e51ef9
Victor Zuluaga <zrvictor@outlook.com>
2020-06-26 00:27:07 +0000

    Capital string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * cap_string - check the code for Holberton School students.
+ * @s: Argument pointer to char
+ *
+ * Return: Always 0.
+ */
+
+char *cap_string(char *s)
+{
+	int i;
+	int p = 32;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (((s[i] > 97) && (s[i] < 122))
+			&& ((s[i - 1] == ' ') || (s[i - 1] == '\n')
+			|| (s[i - 1] == ',') || (s[i - 1] == ';')
+			|| (s[i - 1] == '.') || (s[i - 1] == '!')
+			|| (s[i - 1] == '?') || (s[i - 1] == '"')
+			|| (s[i - 1] == '(') || (s[i - 1] == ')')
+			|| (s[i - 1] == '{') || (s[i - 1] == '}')))
+		{
+			s[i] = s[i] - p;
+		}
+		else
+		{
+			s[i] = s[i];
+		}
+	}
+return (s);
+}
