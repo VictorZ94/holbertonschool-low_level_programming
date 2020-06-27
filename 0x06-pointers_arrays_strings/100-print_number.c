50131008d4619297b2dc49ef62baf05e72545e70
Victor Zuluaga <zrvictor@outlook.com>
2020-06-27 03:11:25 +0000

    added prototype

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -8,14 +8,14 @@
  */
 void print_number(int n)
 {
-      if (n < 0)
-      {
-            _putchar('-');
-            n = -n;
-      }
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
 
-      if (n / 10 != 0)
-            print_number(n / 10);
+	if (n / 10 != 0)
+		print_number(n / 10);
 
 _putchar(n % 10 + '0');
 }
