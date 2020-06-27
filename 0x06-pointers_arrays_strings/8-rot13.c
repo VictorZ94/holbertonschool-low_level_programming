697bc824d9cfc043f0adf825c697f76eaeae9b3e
Victor Zuluaga <zrvictor@outlook.com>
2020-06-27 00:29:41 +0000

    rot13 - change character every 13 positions

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * rot13 - Modify charaters every 13 positions.
+ * @s: Argument pointer to char
+ *
+ * Return: Pointer to char.
+ */
+char *rot13(char *s)
+{
+	char str[] = "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz";
+	char root13[] = "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm";
+	int i, j;
+
+	i = 0;
+	while (s[i])
+	{
+		j = 0;
+		while (str[j])
+		{
+			if (s[i] == str[j])
+				s[i] = root13[j];
+
+		j++;
+		}
+	i++;
+	}
+return (s);
+}
