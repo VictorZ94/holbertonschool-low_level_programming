0be9d4771e3dca0ca2dbcf211a83e7ac07d48930
Victor Zuluaga <zrvictor@outlook.com>
2020-06-30 00:15:14 +0000

    set string

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * set_string- Dereferencing doble pointer.
+ * @s: Doble pointer
+ * @to: pointer to char
+ *
+ * Return: Always 0.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
