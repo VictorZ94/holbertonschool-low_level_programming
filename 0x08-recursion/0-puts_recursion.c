5181711e75975a522160ab674cddabfc934422a4
Victor Zuluaga <zrvictor@outlook.com>
2020-07-01 23:27:01 +0000

    Put with recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,14 +7,12 @@
  */
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	if (s[i] == '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
 
-	_putchar(s[i]);
+	_putchar(*s);
 	_puts_recursion(s + 1);
 }
