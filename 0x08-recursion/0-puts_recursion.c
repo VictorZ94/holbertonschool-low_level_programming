dc726b5a385e9d4b4d387793c9b7d8f64fb24845
Victor Zuluaga <zrvictor@outlook.com>
2020-07-01 23:19:35 +0000

    Recusion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _putchar_recursion - to print only strings
+ * @s: String full
+ * Return: Nothing
+ */
+void _puts_recursion(char *s)
+{
+      int i = 0;
+      if (s[i] == '\0')
+      {
+            _putchar('\n');
+            return;
+      }
+
+      _putchar(s[i]);
+      _puts_recursion(s + 1);
+}
