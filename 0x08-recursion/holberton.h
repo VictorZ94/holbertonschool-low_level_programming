c28077e6744140248295b63aa59c862e3b5b9bc4
Victor Zuluaga <zrvictor@outlook.com>
2020-07-03 02:30:36 +0000

    Added prototype

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -1,2 +1,10 @@
 int _putchar(char c);
 void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int find_second(int n, int i);
+int is_palindrome(char *s);
+int find_palindrome(char *s, int start, int end);
