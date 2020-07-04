1d34359edcdc5bb91900725d3c85e98014f81d57
Victor Zuluaga <zrvictor@outlook.com>
2020-07-04 19:09:01 +0000

    to count the amount of arguments passed through command line interpreter

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - arguments to main
+ * @argc: first arg is a count
+ * @argv: second arg is a pointer to char
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	printf("%d\n", --argc);
+
+return (0);
+}
