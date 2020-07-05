2a89757dd662e4e2425a3850ee465c1bab2d2c65
Victor Zuluaga <zrvictor@outlook.com>
2020-07-05 22:46:02 +0000

    add all arguments

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - arguments to main
+ * @argc: first arg is a count
+ * @argv: second arg is a pointer to char
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, j; 
+      int add = 0;
+
+      i = 0;
+      while(argv[i])
+      {
+            j = 0;
+            while(argv[i][j])
+            {
+                  if ((isdigit(argv[i][j]) == 1))
+                  {
+                        printf("error\n");
+                        return (1);
+                  }
+            j++;
+            }
+      i++;
+      }
+      i = 1;
+      while (argv[i] != argv[argc])
+      {
+           add += atoi(argv[i]);
+      i++;
+      }
+
+      printf("%d\n", add);
+return (0);
+}
