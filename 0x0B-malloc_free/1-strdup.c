36d173981e2dec527ccd53a2836633320a050e8c
Victor Zuluaga <zrvictor@outlook.com>
2020-07-07 22:50:14 +0000

    duplicate a string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,32 +2,33 @@
 #include <stdlib.h>
 
 /**
- * 
- * 
- * 
+ * _strdup - Duplicate a string in a new address memory
+ * @str: string to duplicate
+ *
+ * Return: pointer to char
  */
 char *_strdup(char *str)
 {
-      char *p;
-      int i, j ;
+	char *p;
+	int i, j;
 
-      if (str == '\0')
-            return ('\0');
+	if (str == '\0')
+		return ('\0');
 
-      i = 0;
-      while (str[i])
+	i = 0;
+	while (str[i])
 	{
 		i++;
 	}
-      p = malloc(sizeof(char) * i); /* is not necesary to put space to NULL */
-      if (!p)
-            return ('\0');
+	p = malloc(sizeof(char) * i); /* is not necesary to put space to NULL */
+	if (!p)
+		return ('\0');
 
-      j = 0;
-      while (j <= i)
-      {
-            p[j] = str[j];
-            j++;
-      }
+	j = 0;
+	while (j <= i)
+	{
+		p[j] = str[j];
+		j++;
+	}
 return (p);
 }
