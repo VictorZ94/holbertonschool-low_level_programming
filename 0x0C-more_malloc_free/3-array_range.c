3d5238ef0ec2a807a31d7eaa29ae53043c985844
Victor Zuluaga <zrvictor@outlook.com>
2020-07-09 18:56:56 +0000

    create a new array

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * array_range - Create a new space array
+ * @min: the address of memory to print
+ * @max: the size of the memory to print
+ *
+ * Return: Return pointer.
+ */
+int *array_range(int min, int max)
+{
+	int *p;
+	int i;
+	int size = max - min;
+
+	if (min > max)
+		return (NULL);
+
+	p = malloc(sizeof(int) * size + 1);
+
+	if (!p)
+		return (NULL);
+
+	for (i = 0; i <= size; i++)
+	{
+		p[i] = min++;
+	}
+return (p);
+}
