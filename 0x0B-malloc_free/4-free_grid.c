c897651b04a52d62cb1260f2ce5fb2a3bb2458d8
Victor Zuluaga <zrvictor@outlook.com>
2020-07-09 00:30:27 +0000

    free grid allocated

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - prints a grid of integers
+ * @grid: the address of the two dimensional grid
+ * @height: height of the grid
+ *
+ * Return: Nothing.
+ */
+
+void free_grid(int **grid, int height)
+{
+	int j = 0;
+
+	for (j = 0; j < height; j++)
+	{
+		free(grid[j]);
+	}
+free(grid);
+}
