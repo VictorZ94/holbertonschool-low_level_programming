d937ad3c90d66bf25db04f4c4e19dcecd296b0be
Victor Zuluaga <zrvictor@outlook.com>
2020-07-13 23:56:55 +0000

    struct my dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef MY_STRUCT_H
+#define MY_STRUCT_H
+/**
+ * struct dog - structure defined to set my dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: features of my dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* ifndef MY_STRUCT_H   */
