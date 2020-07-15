620334737a162860bbe8c9cae1b055030cf670f0
Victor Zuluaga <zrvictor@outlook.com>
2020-07-15 19:52:49 +0000

    added prototype

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,9 @@
 #ifndef FUN_POINTER_H
 #define FUN_POINTER_H
-
+#include <stdio.h>
 
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 
 #endif /* ifndef FUN_POINTER_H  */
