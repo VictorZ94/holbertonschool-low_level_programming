420c85c8caa92cce814fdec2acba953c9ad1631a
Victor Zuluaga <zrvictor@outlook.com>
2020-07-17 21:04:28 +0000

    print numbers with separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -3,8 +3,9 @@
 #include "variadic_functions.h"
 
 /**
- * sum_them_all - function sum all arguments.
+ * print_numbers - function num one by one all arguments.
  * @n: n arguments
+ * @separator: characters as separator
  *
  * Return: Always 0.
  */
@@ -19,7 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	if (separator == NULL)
 		return;
-		
+
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
