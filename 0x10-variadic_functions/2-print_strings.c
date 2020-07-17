38d5ae12bbdf1c94dbd93881d9fb1c4eb0abd064
Victor Zuluaga <zrvictor@outlook.com>
2020-07-17 21:18:54 +0000

    prints n strings with separators

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - function num one by one all arguments.
+ * @n: n arguments
+ * @separator: characters as separator
+ *
+ * Return: Always 0.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	char *str;
+	size_t i;
+
+	if (n == 0)
+		return;
+
+	if (separator == NULL)
+		return;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(args, char *);
+		printf("%s", str);
+		if (i != (n - 1))
+			printf("%s", separator);
+	}
+putchar('\n');
+va_end(args);
+}
