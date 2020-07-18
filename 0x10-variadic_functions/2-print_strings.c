a50b898e74eedb091df2ce32b187322f487ca614
Victor Zuluaga <zrvictor@outlook.com>
2020-07-18 23:37:00 +0000

    print all strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,7 +22,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(args, char *);
-		if (string != '\0')
+		if (str != '\0')
 			printf("%s", str);
 
 		else
