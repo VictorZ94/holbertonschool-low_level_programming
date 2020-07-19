bac8e8e62865ee0935946ebc118a326f2fa2e49d
Victor Zuluaga <zrvictor@outlook.com>
2020-07-19 00:42:21 +0000

    print all arguments

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -15,6 +15,8 @@ void print_all(const char * const format, ...)
 	float d;
 	char *str, *separador = "";
 
+	if (!format)
+		return;
 	va_start(args, format);
 	count = 0;
 	while (format[count] != '\0')
