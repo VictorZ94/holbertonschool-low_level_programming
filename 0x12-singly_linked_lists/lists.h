c8569a2147f084ea60639817b33ee3e12bf46eef
Victor Zuluaga <zrvictor@outlook.com>
2020-07-23 20:26:32 +0000

    prototypes linked list

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,21 @@
+#ifndef LIKED_LIST_H
+#define LIKED_LIST_H
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+size_t print_list(const list_t *h);
+
+#endif
