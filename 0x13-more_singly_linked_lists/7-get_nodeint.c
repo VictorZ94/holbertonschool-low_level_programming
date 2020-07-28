93b53a9bbbb81357fad5d0e38a1e302f542fdf28
VictorZ94 <zrvictor@outlook.com>
2020-07-28 19:06:29 +0000

    get node index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,12 +12,12 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	size_t i = 0;
+	if (head == NULL)
+		return (NULL);
 
-	while (i < index)
+	while (index--)
 	{
 		head = head->next;
-		i++;
 	}
 return (head);
 }
