d4006e39079b4420410761927fffb9dcb6080841
VictorZ94 <zrvictor@outlook.com>
2020-07-28 23:28:15 +0000

    insert node int

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -34,6 +34,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	while (--idx)
 	{
 		tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);
 	}
 	newnode->next = tmp->next;
 	tmp->next = newnode;
