1aa6a2c5a39a41c3c2b2b9cc76d3f86d24b91486
VictorZ94 <zrvictor@outlook.com>
2020-07-28 16:34:44 +0000

    free list int 2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,9 +13,9 @@ void free_listint2(listint_t **head)
 
 	while (*head != NULL)
 	{
-		*head = (*head)->next;
-		free(tmp);
-		tmp = *head;
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
 	}
 *head = NULL;
 }
