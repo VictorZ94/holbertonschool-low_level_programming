ef05bc9fd22ded1210e283e8f7928de1d2156344
VictorZ94 <zrvictor@outlook.com>
2020-07-28 23:42:43 +0000

    added prototype

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,6 @@
 #ifndef LINKED_LIST_H
 #define LINKED_LIST_H
-
+#include <stdio.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -25,5 +25,6 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *reverse_listint(listint_t **head);
 
 #endif
