3cb66b6b408d39f31e67897749b2c3c817b460f0
VictorZ94 <zrvictor@outlook.com>
2020-08-04 22:15:45 +0000

    read text file and print

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+/**
+ * 
+ * 
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+      int fd, fdr, fdw;
+      char *buf = malloc(sizeof(char) * letters);
+
+      fd = open(filename, O_RDONLY);
+      if(fd == -1)
+            return (0);
+
+      fdr = read(fd, buf, letters);
+      if (fdr == -1)
+            return (0);
+
+      fdw = write(STDOUT_FILENO, buf, fdr);
+      if (fdw == -1)
+            return (0);
+      close(fd);
+
+free(buf);
+return (fdr);
+}
