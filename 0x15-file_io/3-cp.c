f7aabbe9ab8dfdd0c8a7bb3c64f5853b40d59a25
VictorZ94 <zrvictor@outlook.com>
2020-08-05 20:32:58 +0000

    copy content of a file in another.

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,70 @@
+#include "holberton.h"
+#define BUFFER 1024
+
+/**
+ * main - Enter point with arguments to main.
+ * @argc: counter of arguments
+ * @argv: doble pointer to pointer
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	copy_content(argv[1], argv[2], argv);
+	return (0);
+}
+/**
+ * copy_content - copy content of a file in another.
+ * @file_from: pathname of a first file
+ * @file_to: file of destine
+ * @argv: doble pointer to pointer
+ *
+ * Return: 1 on success, otherwise -1.
+ */
+ssize_t copy_content(const char *file_from, const char *file_to, char **argv)
+{
+	int fd1, fd2, fdr, fdw;
+	char BUF[1024];
+
+	if (!file_from)
+		return (-1);
+
+	if (!file_to)
+		return (1);
+
+	fd1 = open(file_from, O_RDONLY);
+	if (fd1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	fdr = read(fd1, BUF, BUFFER);
+	if (fdr == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (close(fd1) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
+		exit(100);
+	}
+	fd2 = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd2 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+
+	fdw = write(fd2, BUF, fdr);
+	if (fdw == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+
+	if (close(fd2) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2), exit(100);
+
+	return (1);
+}
