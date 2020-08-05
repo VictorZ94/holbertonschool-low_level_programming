f1b744e16465d540196fc48bbaec744584874441
VictorZ94 <zrvictor@outlook.com>
2020-08-05 20:32:21 +0000

    added prototypes

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -10,4 +10,6 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+ssize_t copy_content(const char *filename, const char *, char **);
+
 #endif
