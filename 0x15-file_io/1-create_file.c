9223f87c73daaca7aec2f3d25f670fce3fe533f6
VictorZ94 <zrvictor@outlook.com>
2020-08-05 00:17:57 +0000

    create a file and put it text

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * create_file - read and print it filename
+ * @filename: pathname
+ * @text_content: numbers of a character to save
+ *
+ * Return: 1 on success, otherwise -1.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, fdw;
+	int counter;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	counter = 0;
+	while (text_content[counter])
+		counter++;
+
+	fdw = write(fd, text_content, counter);
+	if (fdw == -1)
+		return (-1);
+
+close(fd);
+return (1);
+}
