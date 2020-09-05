041b3cff7c30d117f3b1b8082e8ebafcc5a53041
VictorZ94 <zrvictor@outlook.com>
2020-09-05 21:42:06 +0000

    sum all nodes data int

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - sum all nodes data int.
+ * @head: Pointer at top of the nodes
+ * Return: adds
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+	int sum;
+
+	if (!head)
+		return (NULL);
+
+	current = head;
+	while (current->next != NULL)
+		current = current->next;
+
+	while (current->prev != NULL)
+	{
+		sum += current->n;
+		current = current->prev;
+	}
+	return (sum);
+}
