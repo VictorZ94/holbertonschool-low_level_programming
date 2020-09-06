278792ebd789972f50f6d64ea50b17690b3e2613
VictorZ94 <zrvictor@outlook.com>
2020-09-06 03:59:47 +0000

    all main to test my code

---
+++ 0x17-doubly_linked_lists/6-main.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+    dlistint_t *head;
+    int sum;
+
+    head = NULL;
+    add_dnodeint_end(&head, 1024);
+    add_dnodeint_end(&head, 6);
+    add_dnodeint_end(&head, 40);
+    sum = sum_dlistint(head);
+    printf("sum = %d\n", sum);
+    free_dlistint(head);
+    head = NULL;
+    return (EXIT_SUCCESS);
+}
