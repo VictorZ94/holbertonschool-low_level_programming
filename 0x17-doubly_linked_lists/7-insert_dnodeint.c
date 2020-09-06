45d130ae4eb28f6d918edef6a3370266680e5a86
VictorZ94 <zrvictor@outlook.com>
2020-09-06 04:21:30 +0000

    insert node at the end bug: temp it was staying out NULL

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -35,7 +35,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		if (!current)
 			return (NULL);
 	}
-	temp = current->next;
+	if ((*current).next != NULL)
+		temp = (*current).next;
 	(*NewNode).next = (*current).next;
 	(*NewNode).prev = current;
 	(*current).next = NewNode;
