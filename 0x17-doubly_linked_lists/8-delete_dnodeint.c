a1bd794869a94d7107ca91a9bfce07df21ad15fd
VictorZ94 <zrvictor@outlook.com>
2020-09-06 00:28:14 +0000

    delete node

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - Insert node at the index.
+ * @head: Pointer at top of the nodes
+ * @index: position to free
+ * Return: On succes 1 otherwise -1.
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *current;
+	dlistint_t *DeleteNode;
+
+	if (!head || !*head)
+		return (-1);
+
+	current = *head;
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(current);
+		return (1);
+	}
+	while (--index)
+	{
+		current = current->next;
+		if (current == NULL)
+			return (-1);
+	}
+	DeleteNode = current->next;
+	current->next = DeleteNode->next;
+	DeleteNode->next->prev = current;
+	free(DeleteNode);
+	return (1);
+}
