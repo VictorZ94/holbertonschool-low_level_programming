77e6b37cdb17d2d5c7a6cc876eeab7bd7fefd2ec
Victor Zuluaga <zrvictor@outlook.com>
2020-10-02 22:27:44 -0500

    hash table

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,16 +11,17 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index = 0;
 
+
 	if (!ht || !key || strlen(key) == 0)
 		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	if (ht->array[index])
+	while (ht->array[index])
 	{
 		if ((strcmp(ht->array[index]->key, key)) == 0)
-		{
 			return (ht->array[index]->value);
-		}
+
+		ht->array[index] = ht->array[index]->next;
 	}
 	return (NULL);
 }
