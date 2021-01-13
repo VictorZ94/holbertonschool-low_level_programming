498c3807e78a0b14526bcae8ca174e18b48a3507
Victor Zuluaga <zrvictor@outlook.com>
2021-01-13 18:00:12 -0500

    Jump searching The fundamental idea behind this searching technique is to search fewer number of elements compared to linear search algorithm

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,40 @@
+#include "search_algos.h"
+
+/**
+ * jump_search - search sorting array jumping quater root of size
+ * @array: list all the elements
+ * @size: size of array or list
+ * @value: matches with some value within array
+ *
+ * Return: index if value matches. Otherwise: -1
+ *
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+	size_t skip = 0, x = 0;
+
+	skip = sqrt(size);
+	while (i < size)
+	{
+		if (array[i] >= value)
+		{
+			x = (i - sqrt(size)) + 1;
+			printf("Value found between indexes [%lu] and [%lu]\n", x, i);
+			while (x <= i)
+			{
+				printf("Value checked array[%ld] = [%d]\n", x, array[x]);
+				if (array[x] == value)
+					return (x);
+				x++;
+			}
+		}
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		i = skip;
+		skip = skip + sqrt(size);
+	}
+	x = (i - sqrt(size)) + 1;
+	printf("Value found between indexes [%lu] and [%lu]\n", x, i);
+	printf("Value checked array[%ld] = [%d]\n", x, array[x]);
+	return (-1);
+}
