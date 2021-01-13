1e4707401a6a1e5d7f333fea4394ccbe941e644c
Victor Zuluaga <zrvictor@outlook.com>
2021-01-13 18:01:19 -0500

    prototypes search algorithm

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,9 +2,13 @@
 #define SEARCHING_ALGORITHM_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-
+int binarySearch(int arr[], int l, int r, int x);
+int jump_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
 
 #endif
